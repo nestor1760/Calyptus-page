0a3cbfae2b75e9d3ad0eb239d62350e09e8f6c3f
Roman Nesterchuk <133716724+nestor1760@users.noreply.github.com>
2024-03-05 21:24:24 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,18 +5,6 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './store/store';
 
-// const firebaseConfig = {
-//     apiKey: "AIzaSyBRGMULXiV97IPAtfg6Rypn4OCfC3Gv1Qs",
-//     authDomain: "calyptus-store-eb7ce.firebaseapp.com",
-//     projectId: "calyptus-store-eb7ce",
-//     storageBucket: "calyptus-store-eb7ce.appspot.com",
-//     messagingSenderId: "767761814352",
-//     appId: "1:767761814352:web:a33118912a9bacc3151255",
-//     measurementId: "G-L2KQFSSH3Z"
-//   };
-
- 
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <Provider store={store}>
